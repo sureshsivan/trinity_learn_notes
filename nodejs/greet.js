1b5e38d2a65925c504e7429d8667551e4a195c9b
Suresh Sivanantham <Suresh Sivanantham>
2016-03-27 22:43:31 -0700

    more code

--- nodejs/greet.js
+++ nodejs/greet.js
@@ -1,12 +1,16 @@
 
-console.log(exports);
-console.log(module.exports);
-console.log(exports === module.exports);
+// console.log(exports);
+// console.log(module.exports);
+// console.log(exports === module.exports);
 
-exports = module.exports = function(){
-    console.log('inside exports');
+// exports = module.exports = function(){
+//     console.log('inside exports');
+// }
+
+// console.log(exports);
+// console.log(module.exports);
+// console.log(exports === module.exports);
+
+export function greet(){
+    console.log("Greeting from ES6");
 }
-
-console.log(exports);
-console.log(module.exports);
-console.log(exports === module.exports);
--- nodejs/index.js
+++ nodejs/index.js
@@ -1,2 +1,30 @@
-var a = require("./greet");
-console.log(">>> ", a);
+// var a = require("./greet");
+// var util = require("util");
+// console.log(">>> ", a);
+
+// import * as greeter from "greet"
+
+// greeter.greet();
+
+
+// var name = "suresh";
+// var greetOlder = "Hello : " + name;
+// var greetEs6 = `Hello : ${name}`;	//	can be multi line
+// console.log(greetOlder);
+// console.log(greetEs6);
+
+// var alphanumeric = new Buffer("H", "utf8");
+// var alphanumericJap = new Buffer("ぁ", "utf8");
+
+// console.log(alphanumeric);
+// console.log(alphanumericJap)
+
+
+var fs = require('fs');
+var readable = fs.createReadStream(__dirname + '/greet.txt', { encoding: 'utf8', highWaterMark: 16 * 1024 });
+var writable = fs.createWriteStream(__dirname + '/greetcopy.txt');
+
+readable.on('data', function(chunk) {
+	writable.write(chunk);
+});
+
